8ed9c73e9f805069cd14e17e6503dc7e132e5ee4
tazim04 <35643104+tazim04@users.noreply.github.com>
2026-01-03 18:29:46 -0500

    Added more aggregates to schema

--- src/main/resources/db/migration/V2__crime_grid_aggregation.sql
+++ src/main/resources/db/migration/V2__crime_grid_aggregation.sql
@@ -1,8 +1,23 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS crime_stats_grid AS
+CREATE MATERIALIZED VIEW crime_stats_grid AS
 SELECT
     ST_SnapToGrid(location, 0.01) AS grid,
+
     COUNT(*) AS total_crimes,
-    MODE() WITHIN GROUP (ORDER BY offence_category) AS most_common_crime
+
+    COUNT(*) / GREATEST(
+            DATE_PART('year', MAX(reported_date) - MIN(reported_date)),
+            1
+               ) AS avg_crimes_per_year,
+
+    COUNT(*) FILTER (
+        WHERE reported_date > NOW() - INTERVAL '1 year'
+        ) AS crimes_last_year,
+
+    MODE() WITHIN GROUP (ORDER BY offence_category) AS most_common_crime,
+
+    MIN(reported_date) AS first_reported,
+    MAX(reported_date) AS last_reported
+
 FROM crime_records
 GROUP BY grid;
 
